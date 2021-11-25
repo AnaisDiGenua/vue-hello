67c778b0a3e38f2d383ff3e2984e743f24d51f5b
AnaisDiGenua <anais.digenua5@gmail.com>
2021-11-25 18:46:59 +0100

    print message

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        message: 'hello vue',
+        image: 'https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg',
+        position: 'center'
+    }
+});
